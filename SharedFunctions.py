0d87ad3a2f24a12285c166f353d1240d2c9cdd18
Lawrence Abu-Jaber <71214072+labujaber1@users.noreply.github.com>
2022-10-26 12:04:17 +0100

    Further developed filtering lists.

--- SharedFunctions.py
+++ SharedFunctions.py
@@ -1,8 +1,7 @@
 from ast import Break, Return
-from cgitb import text
-import collections
 from operator import countOf
 import re
+from xmlrpc.client import Boolean
 import numpy as np
 import pandas as pd
 
@@ -16,11 +15,12 @@ def Get_WordFamilyList(wordLen):
         my_file = open("dictionary.txt","r")
         wordFamilyList  = my_file.read().split()
         my_file.close()
+        print(f"Original list length = {len(wordFamilyList)}")
         new_list = [ word for word in wordFamilyList if len(word) == wordLen ]
         return new_list
     except FileNotFoundError:
         print(f"Dictionary file not found.")
-        Return
+        return
     
     
 
@@ -35,55 +35,70 @@ def Algor_Method(e,f):
 
 
 # not dependant on diff level so can be used by both algorithms
-def Partition_Words(wordList,wordLength,letterGuess,letterIndex):
+# pass in a word list and filter according to the user letter guess
+# return new filtered word list with either a successful or failed guess result.
+def Filter_wordList(wordList,wordLength,letterGuess):
+    # setup list to use
+    wordList1: list = wordList
+    functionComplete: Boolean = False #false
+    # make new word list from param list reset length checker to zero
+    paramWordListLen = 0
+    wordList1Len = 0
     
-    # get list length
-    wordListLen = len(wordList)
-    print(f"Total number of words in the list is {wordListLen}")
+    ####  get list length used in testing to check word list each cycle
+    paramWordListLen = len(wordList)
+    wordList1Len = len(wordList1)
+    print(f"Total number of words in the param list is {wordList1Len}\nnew list length is {paramWordListLen}\n")
+    # check they match and nothing included from memory for testing
+    res1 = wordList1Len - paramWordListLen
+    if res1 != 0:
+        raise Exception ("Length of word lists do not match, cannot proceed\n")
+    ### delete above when testing complete 
 
-    # works to give count of words do not contain a letterGuess
-    '''res0 = len(list(filter(lambda ele : letterGuess not in ele, wordList)))
-    print(f"Occurrence for no letter '{letterGuess }' = {res0}")'''
-    
-    # gives count of all words contain a letterGuess 
-    '''res1 = len(list(filter(lambda ele : letterGuess in ele, wordList)))
-    print(f"Occurrence for one letter '{letterGuess }' = {res1}")'''
-    
-    # Append count totals to a list of all occurrences of letterGuess between 0-3
+    ## ------ FIRST LIST SPLIT SECTION -------- ##
+    ## ---- RULE: pick largest group of words that contain letter
+    # COUNT THEN ADD TO LIST to save memory space and reduce process time 
+    # Append COUNT totals to a list of all occurrences of letterGuess between 0-3
+    # how many times letter ie 'e' appears 0,1,2,3 times any more is no point
     bool = True
     index2 = 0
     listTotal = []
     while bool == True:
         if index2 < 4:
-            listOccurTotal = len(list(word for word in wordList if countOf(word,letterGuess) == index2))
+            listOccurTotal = len(list(word for word in wordList1 if countOf(word,letterGuess) == index2))
             listTotal.append(listOccurTotal)
             #print(f"No. of letters = {index2}, and total words = {listTotal}")
             index2 += 1
         else:
             bool = False
-    print(f"No. of letters = {index2}, and total words = {listTotal}")
-    print(f"Number of word groups should equal 4 (letterGuess from 0-3): {len(listTotal)}") 
-    #print(targetList)
-    
+    print(f"Total times the letter {letterGuess} appears in word 0-3 times = {listTotal}")
     # select the largest count occurrence of the letterGuess param
-    letterOccur = listTotal.index(max(listTotal))
+    letterOccur = int(listTotal.index(max(listTotal)))
     
     # if largest list of letter occurrence == zero (not contain the letter guessed)
     # then RETURN 'letter not guessed'
     if letterOccur == 0:
         ''' End function '''
-        return print("letter not guessed")
+        # add all words to list and return
+        wordList1 = [word for word in wordList1 if countOf(word,letterGuess) == letterOccur]
+        print("letter not in word, try again")
+        return wordList1,letterOccur,functionComplete
         
+
+
+    ## ------- SECOND LIST SPLIT SECTION -------- ##
+    ## ----- RULE: Pick largest group of words where the index of the letter appears
+    # COUNT THEN ADD TO LIST to save memory space and reduce process time
+
     targetWordList = []
     # get all words from wordList param that is in the largest group of occurrences 
-    # if not equal to zero. Add to new list targetWordList
+    # if not equal to zero. ADD to new list targetWordList
     if letterOccur > 0: 
-        targetWordList = [word for word in wordList if countOf(word,letterGuess) == letterOccur]
+        targetWordList = [word for word in wordList1 if countOf(word,letterGuess) == letterOccur]
         print(f"Word length: {wordLength}, Max list of words for letter {letterGuess}: {len(targetWordList)}")
         print(f"Index {letterOccur = }") 
-    
-    # count occurrence of guessed letter for each index position in each word in the list
-    # ------- SPLIT SECTION --------
+        
+    # get count occurrence of guessed letter for each index position in each word in the list
     bool = True
     idx = 0 
     count = 0
@@ -98,26 +113,42 @@ def Partition_Words(wordList,wordLength,letterGuess,letterIndex):
             idx+=1
             res.append(count)
             count = 0
-            #print(f"{res=}")
         else:
             bool = False
 
     print(f"Letter occurrence in split list: {res}")
 
-    letterOccurSplit = res.index(max(res))
+    # create list of words that have largest group of words where
+    # letterGuess is in a certain position within wordLength
+    letterOccurSplitIdx = res.index(max(res))
     targetWordListSplit = []
-    targetWordListSplit = [word for word in targetWordList if word[letterOccurSplit] == letterGuess]
+    targetWordListSplit = [word for word in targetWordList if word[letterOccurSplitIdx] == letterGuess]
     print(f"Word length still: {wordLength}, Max list of words for letter {letterGuess}: {len(targetWordListSplit)}")
-    print(f"Index {letterOccurSplit = }") 
-    # remove all from targetWordList that does not equal largest group where letterGuess 
-    # is in position index
-
-    # replace original param list wordList with end result targetWordList to process 
+    print(f"Index {letterOccurSplitIdx = }") 
+    #if completed with letter return true else already returned false line 89
+    functionComplete = True #true
+    # replace original param list wordList with end result targetWordListSplit to process 
     # newly entered letterGuess from user
-    # clear all lists 
-    # RETURN index position, word list, guessed letter
-    ## need to check positions of letterGuess what happens if list is 
-    ## chosen that contains 2 letters as the counts look for 1 occur
+    
+    # clear PREVIOUS USED temp word lists ready to pass in for next cycle  
+    wordList1.clear()
+    targetWordList.clear()
+    print("Ending Filter_WordList function")
+    # RETURN new word list,index position of successful letter, and function complete marker (true/false)
+    return (targetWordListSplit,letterOccurSplitIdx,functionComplete)
     ## index position used to display guessed letter in wordDisplay var for user
     
+# Add successful letter guessed to word to display to user 
+# index out of range for successful last letter and not adding entire word length,
+# and not concatenating successful guesses, bit of a cock up really!  
+def addLetterToWordDisplay(wordDisplay,letterIndex,letterGuess):
+    print(f"Printing word display before trying to add letters -> {wordDisplay}")
+    word = wordDisplay
+    #word = word[:letterIndex]+letterGuess+word[letterIndex]+wordLength-letterIndex
+    word = word[:letterIndex]+letterGuess+word[letterIndex]
+    print("Ending addLetterToWordDisplay function")
+    return word
+
+
+
 
--- WordGuess.py
+++ WordGuess.py
@@ -1,34 +1,32 @@
-
 from ast import Return
 from dataclasses import dataclass
 from datetime import datetime
-
 import random
-from SharedFunctions import Get_WordFamilyList,Algor_Method, Partition_Words
+from SharedFunctions import Get_WordFamilyList, Algor_Method, Filter_wordList, addLetterToWordDisplay
 
 @dataclass() #(frozen=True, order=True)
 class WordGuess:
-    setLevel: str = "easy"
-    gameComplete: bool = False
-    guesses: int = 0
+    setLevel = "easy"
+    gameComplete = False
+    guesses = 0
     wordFamilyList = []
-    wordLength: int = 0
-    CallAlgorithm: str = ""
+    wordLength = 0
+    CallAlgorithm = ""
     usedLetters = []
-    wordDisplay: str = ""
-    letterGuess: str = ""
-    letterIndex: int = 0
+    wordDisplay = ""
+    letterGuess = ""
+    letterIndex = 0
 
 
 
-    def setup(self):
+    def setupLevel(self):
         # getting the word size for word list and double for number of guesses
         self.wordLength = random.randrange(4,12)
         self.guesses = self.wordLength*2
 
         #open text file and save to list
         self.wordFamilyList = Get_WordFamilyList(self.wordLength)
-
+        print(f"Main list filtered by word length {self.wordLength} \nand returned new list length {len(self.wordFamilyList)}")
         # asking user to set the level for the game.
         while True:
             answer = input("Would you like to play on easy mode y/n: ").lower()
@@ -47,32 +45,60 @@ class WordGuess:
     def wordGuess_game(self):
         # set algorithm to use and call when ever its actually created add letterGuess as param
         self.CallAlgorithm = Algor_Method(self.setLevel,self.wordFamilyList)
-        self.usedLetters = ['a']
-        self.wordDisplay = ['-'*self.wordLength]
+        self.usedLetters = []
+        self.wordDisplay = '-'*self.wordLength
 
         # put in while loop when ready 
-        #loop = False
-        #while game_complete == False
-        print("You have {0} guesses left".format(self.guesses))
-        print("Used letters so far: {0}".format(self.usedLetters))
-        print("Word: ",self.wordDisplay)
-        while True:
-            self.letterGuess = input("Enter a letter: ").lower()
-            if self.letterGuess.isalpha() == True & len(self.letterGuess) == 1:
-                # if letter not already guessed then append
-                if self.letterGuess in self.usedLetters:
-                    print("This has already been guessed try again")
-                    Return
+        game_complete = False
+        while game_complete == False:
+            print("You have {0} guesses left".format(self.guesses))
+            print("Used letters so far: {0}".format(self.usedLetters))
+            print("Word: ",self.wordDisplay)
+            
+            while True:
+                self.letterGuess = input("Enter a letter: ").lower()
+                if self.letterGuess.isalpha() == True & len(self.letterGuess) == 1:
+                    # if letter not already guessed then append
+                    if self.letterGuess in self.usedLetters:
+                        print("This has already been guessed try again")
+                        Return
+                    else:
+                        print(f"WordList length before function wordGuess line66 = {len(self.wordFamilyList)}")
+                        # put partition_words function here?
+                        # returning targetWordListSplit,letterOccurSplit,functionComplete
+                        returnStuff = Filter_wordList(self.wordFamilyList,self.wordLength,self.letterGuess)
+                        self.wordFamilyList = returnStuff[0]
+                        letterGuessIdx = returnStuff[1]
+                        functionComplete = returnStuff[2]
+                        print(f"WordList length after function = {len(self.wordFamilyList)}")
+                        print(f"Letter occurred index = {letterGuessIdx}, and function complete = {functionComplete}")
+                        # when no letter matched letterOccurred is 0
+                        if functionComplete == False:
+                            self.usedLetters.append(self.letterGuess)
+                            self.guesses -= 1
+                            #print(f"Word display: {self.wordDisplay}")
+                            Return
+                        if functionComplete == True:
+                            wordAmend = addLetterToWordDisplay(self.wordDisplay,letterGuessIdx,self.letterGuess)
+                            self.wordDisplay = wordAmend
+                            self.usedLetters.append(self.letterGuess)
+                            # add 1 to guesses when safe to append letter that has not been guessed already
+                            self.guesses -= 1
+                            print(f"Word display: {self.wordDisplay}")
+                        else:
+                            print("Filter_wordList completeFunction is false")
+                            break
                 else:
-                    self.usedLetters.append(self.letterGuess)
-                    # add 1 to guesses when safe to append letter that has not been guessed already
-                    self.guesses -= 1
-                    break
-            else:
-                print("Eh computer says no, please try again")
+                    print("Eh computer says no, please try again")
+            if self.guesses == 0:
+                self.wordGuess_end()
+                
+        #
+        # first run with first guess need to include to above loop 
+        # partition_words function returns 2: list (new word list) and int (letter index)
+        #newWordList,displayWordIndex,done = Partition_Words(self.wordFamilyList,self.wordLength,self.letterGuess,self.letterIndex)
+        # display word if letter guess is accepted
 
-
-        Partition_Words(self.wordFamilyList,self.wordLength,self.letterGuess,self.letterIndex)
         print("Good guess")
 
 
@@ -83,6 +109,11 @@ class WordGuess:
         if "-" not in self.wordDisplay:
             print(self.wordDisplay)
             print("Well done you have guessed the word and beaten the computer")
+        else:
+            return
+
+
+
 
 newGame = WordGuess()
 
@@ -94,10 +125,11 @@ def main():
     print(f"\n{displayDatetime}")
 
     print(f"Welcome to the Word Guess game\n")
-
-    newGame.setup()
+    newGame.setupLevel()
     newGame.wordGuess_game()
-    newGame.wordGuess_end()
+    newGame.wordGuess_end() # only here to finish program while testing
+    
+    
     
 
 # this is how the program starts
